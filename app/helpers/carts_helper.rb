a3d38fe6f86624ec47353c868e84289003bff553
Marwa7246 <marwa.ragheb@gmail.com>
2020-09-20 16:13:57 -0400

    SHow user email on order when login

--- app/helpers/carts_helper.rb
+++ app/helpers/carts_helper.rb
@@ -2,4 +2,21 @@ module CartsHelper
   def empty?
     cart.size < 1  
   end
+
+  def order_description
+    if current_user
+      return "#{current_user.first_name} #{current_user.first_name}'s Jungle Order"
+    else
+      " Jungle Order"    
+    end
+  end
+
+  def order_email
+    if current_user
+      return "#{current_user.email}"
+    else
+      " "
+    end
+  end
+
 end
