cf0e94c332b13541a94fc7c2da1691ed2c492a75
Marwa7246 <marwa.ragheb@gmail.com>
2020-09-20 18:30:42 -0400

    Remove unnecessary comments

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,21 +3,16 @@ class SessionsController < ApplicationController
   def new
   end
 
+  # Save the user id inside the browser cookie.  
+
   def create
     user = User.find_by_email(params[:email])
-    # If the user exists AND the password entered is correct.
     if user && user.authenticate(params[:password])
-      # Save the user id inside the browser cookie. This is how we keep the user 
-      # logged in when they navigate around our website.
       session[:user_id] = user.id
       redirect_to '/'
     else
-    # If user's login doesn't work, send them back to the login form.
-    @error_login='Invalid Username/password!'
-    
-    #puts @error_login
-    #raise @error_login
-      render :new, alert: 'Invalid Username/password! Please Try again'
+      @error_login='Invalid Username/password!'
+      render :new, alert: @error_login
     end
   end
 
