6ccc26de55e8ad36fe822544314d5441fb79a78d
Marwa7246 <marwa.ragheb@gmail.com>
2020-09-21 12:55:35 -0400

    Modify routes names to be consistent

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to '/'
+      redirect_to :root
     else
       @error_login='Invalid Username/password!'
       render :new, alert: @error_login
@@ -18,6 +18,6 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to '/'
+    redirect_to :root
   end
 end
