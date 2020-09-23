f25fb215a4f4f9e23c439be73156966e6007c88c
Marwa7246 <marwa.ragheb@gmail.com>
2020-09-22 22:03:47 -0400

    Refactor welcome email process

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,3 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "no-reply@jungle.com"
-  layout 'mailer'
+
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,16 @@
-class UserMailer < ApplicationMailer
-  default from: "no-reply@jungle.com"
+class UserMailer < ActionMailer::Base
+ default(
 
+   from: "<no-reply@jungle.com>",
+   reply_to: "<no-reply@jungle.com>"
+
+)
+
+  
   def welcome_email(user)
     @user = user
     @url  = 'http://example.com/login'
-    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+    mail(to: @user.email, subject: 'Welcome to Jungle Site')
   end
 
 end
