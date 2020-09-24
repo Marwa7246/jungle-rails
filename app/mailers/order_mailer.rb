4b2ed6c796a2f8750acfca3546d74b04b86de360
Marwa7246 <marwa.ragheb@gmail.com>
2020-09-23 20:52:51 -0400

    Add more comments

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -3,6 +3,7 @@ class OrderMailer < ApplicationMailer
   default from: 'no-reply@jungle.com',
   return_path: 'no-reply@jungle.com'
   
+  ########Send an email to the user if the order is valid with the order details
   def new_order_email(order)
     @order = order
     @line_items = LineItem.where(order: @order.id)
