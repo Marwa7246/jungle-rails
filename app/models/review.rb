be587c501fdd458db27be6d9404a0a15d51212b1
Marwa7246 <marwa.ragheb@gmail.com>
2020-10-18 00:43:24 -0400

    Generate review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,5 @@
 class Review < ActiveRecord::Base
+	validates :user_id, :product_id, :description, :rating, presence: true
+	belongs_to :product
+	belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
 
+  has_many :reviews
+
   validates :email, :first_name, :last_name, presence: true
   validates :password, length: { minimum: 8 }, confirmation: true
   validates :password_confirmation, presence: true 
