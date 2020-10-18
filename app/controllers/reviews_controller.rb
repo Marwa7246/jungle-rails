d9b7e998d86abc8b4ac14eae3db17c61a6304d93
Marwa7246 <marwa.ragheb@gmail.com>
2020-10-18 00:44:23 -0400

    Generate review controller

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,19 @@
+class ReviewsController < ApplicationController
+  def create
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new(review_params)
+    @review.user = current_user
+    if @review.save
+      redirect_to [:root]
+    else
+      redirect_to product_path(@product)
+
+    end
+    byebug
+  end
+
+  private
+  def review_params 
+    params.require(:review).permit(:description, :rating)
+  end
+end
